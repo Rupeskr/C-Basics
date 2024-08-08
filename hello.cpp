f40e62e4746d0fb12fafee276d1c5d7f363fb4f8
Rupesh Kumar <149712049+Rupeskr@users.noreply.github.com>
2024-08-08 12:37:03 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//Take input from user
+#include<iostream>
+using namespace std;
+int main(){
+    int apples;
+    cout<<"Enter no. of apples:";
+    cin>>apples;
+    cout<<"No of apples are:"<<apples<<endl;
+    return 0;
+}
